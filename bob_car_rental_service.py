4702ca5e7583cabdffcff96570fcad7279c2bd05
Ali Nassar <aliinassar8@gmail.com>
2025-02-28 20:32:10 +0200

    Building Car class which is a sub class of Vehicle that will inherit its attributes and methods

--- bob_car_rental_service.py
+++ bob_car_rental_service.py
@@ -9,3 +9,8 @@ class Vehicle:
         print(f"Brand: {self.brand}, Model: {self.model}, Year: {self.year}, Rental Price per Day: ${self.rental_price_per_day}")
     def calculate_rental_cost(self,days):
         return days * self.rental_price_per_day
+
+class Car(Vehicle):
+    def __init__(self,brand,model,year,rental_price_per_day,seating_capacity):
+        super().__init__(brand,model,year,rental_price_per_day)
+        self.seating_capacity = seating_capacity
