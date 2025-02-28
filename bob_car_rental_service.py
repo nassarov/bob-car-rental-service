c6731b7d58f79e2dcd033682cd615f9a9e165cf8
Ali Nassar <aliinassar8@gmail.com>
2025-02-28 20:25:26 +0200

    Defining display_info method

--- bob_car_rental_service.py
+++ bob_car_rental_service.py
@@ -4,3 +4,6 @@ class Vehicle:
         self.model = model
         self.year = year
         self.rental_price_per_day = rental_price_per_day
+
+    def display_info(self):
+        print(f"Brand: {self.brand}, Model: {self.model}, Year: {self.year}, Rental Price per Day: ${self.rental_price_per_day}")
