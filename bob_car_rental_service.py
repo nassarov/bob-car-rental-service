934cb685485a4046782f61791fe3d462162605c0
Ali Nassar <aliinassar8@gmail.com>
2025-02-28 20:35:25 +0200

    Overriding display_info method for Bike class

--- bob_car_rental_service.py
+++ bob_car_rental_service.py
@@ -22,3 +22,6 @@ class Bike(Vehicle):
     def __init__(self,brand,model,year,rental_price_per_day,engine_capacity):
         super().__init__(brand,model,year,rental_price_per_day)
         self.engine_capacity = engine_capacity
+
+    def display_info(self):
+        return print(f"Bike :{self.brand} {self.model}, Year: {self.year}, Engine: {self.engine_capacity}cc, Rental Price: ${self.rental_price_per_day}/day")
