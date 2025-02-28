084bd5da9a9e4a9cbb988911188c749a7a5742f3
Ali Nassar <aliinassar8@gmail.com>
2025-02-28 20:23:56 +0200

    Building class Vehicle with it's attributes

---
+++ bob_car_rental_service.py
@@ -0,0 +1,6 @@
+class Vehicle:
+    def __init__(self, brand, model, year, rental_price_per_day):
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.rental_price_per_day = rental_price_per_day
