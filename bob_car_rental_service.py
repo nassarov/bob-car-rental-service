61a569005040d5759f953741c938339bd944bb90
Ali Nassar <aliinassar8@gmail.com>
2025-02-28 20:36:37 +0200

    Setting rental_price attribute as a private attribute

--- bob_car_rental_service.py
+++ bob_car_rental_service.py
@@ -3,7 +3,7 @@ class Vehicle:
         self.brand = brand
         self.model = model
         self.year = year
-        self.rental_price_per_day = rental_price_per_day
+        self.__rental_price_per_day = rental_price_per_day
 
     def display_info(self):
         print(f"Brand: {self.brand}, Model: {self.model}, Year: {self.year}, Rental Price per Day: ${self.rental_price_per_day}")
